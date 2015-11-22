887bf24e31263eb1453692c8c3ea8b05ac8636c2
WizzardMaker <wizzardmaking@hotmail.com>
2015-11-22 17:22:10 +0100

    KI Wegfindung

--- Assets/Jonas/Skripte/AI.cs
+++ Assets/Jonas/Skripte/AI.cs
@@ -9,19 +9,60 @@ public class AI: MonoBehaviour {
 	public Texture2D selected;
 	bool isSelected = false;
 
+	Transform waypoint;
+
 	public Vector3 goal;
 
 	// Use this for initialization
 	void Start () {
 		cc = GetComponent<CharacterController>();
+		waypoint = transform.GetChild(0);
 	}
-	
+
+	void GetGoal() {
+
+		bool gotTile = false;
+		float moveX = 0;
+		float moveY = 0;
+
+		int breaker = 0;
+
+		while (!gotTile) {
+			breaker++;
+
+			RaycastHit hit;
+
+			if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition + Vector3.forward).origin + new Vector3(moveX, 0, moveY), Camera.main.ScreenPointToRay(Input.mousePosition + Vector3.forward).direction, out hit)) {
+				if (hit.collider.tag == "Waypoint") {
+					//Debug.Log(Random.Range(0, 2));
+					moveX += Random.Range(0, 2) == 1 ? -2 : 2;
+					moveY += Random.Range(0, 2) == 1 ? -2 : 2;
+				} else {
+
+					goal = hit.point;
+					waypoint.position = goal;
+					gotTile = true;
+				}
+			}
+
+			//Debug.Log(breaker + "/" + moveX + "+"+ moveY);
+
+			if (breaker >= 200)
+				break;
+
+		}
+
+	}
+
 	// Update is called once per frame
-	void FixedUpdate () {
+	void Update () {
 		CheckSelected();
 
 		CheckMovement();
-		if(GetComponent<NavMeshAgent>().destination != goal)
+
+		waypoint.position = goal;
+
+		if (GetComponent<NavMeshAgent>().destination != goal)
 			GetComponent<NavMeshAgent>().SetDestination(goal);
 
 		if(isPlayer)
@@ -29,11 +70,11 @@ public class AI: MonoBehaviour {
 	}
 	
 	void CheckMovement() {
+		
 		if (Input.GetMouseButtonUp(1) && isSelected) {
-			RaycastHit hit;
-			if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition + Vector3.forward), out hit))
-				goal = hit.point;
+			GetGoal();
         }
+
 
 	}
 
